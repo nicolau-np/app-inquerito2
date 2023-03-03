e4124c8ca6ead94255a13f91aa66e75e73e50ee5
Nicolau NP <mr1Normaliii@gmail.com>
2023-03-03 18:21:47 +0000

    reaction

--- src/app/components/modals/react/react.component.ts
+++ src/app/components/modals/react/react.component.ts
@@ -1,5 +1,6 @@
 import { ApiRequestService } from './../../../services/api-request.service';
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-react',
@@ -8,15 +9,24 @@ import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 })
 export class ReactComponent implements OnInit {
   @Input() public paren: any
-  reactions: any;
+  reactions: any
 
-  constructor(private api: ApiRequestService) {
-
-  }
+  constructor(private api: ApiRequestService) { }
 
   ngOnInit(): void {
     this.api.index('/reactions/types').subscribe((response: any) => {
       this.reactions = response.data
+    })
+  }
+
+  onReact(id: number) {
+
+  const reaction_comment = {
+      id_reaction_type: id,
+      id_comment: this.paren.id
+    }
+    this.api.store('/reactions', reaction_comment).subscribe((response: any) => {
+
     })
   }
 }
